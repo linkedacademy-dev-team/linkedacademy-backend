36c98ba7934ab42b1b4437a579326efa7abad6c2
DQuintero15 <quinterod164@gmail.com>
2024-03-16 15:26:51 -0500

    feat: add compression middleware for better app performance

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core"
+import * as compression from "compression"
+
+import { AppModule } from "./app.module"
+
+const GLOBAL_PREFIX = "api"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule)
+
+	app.setGlobalPrefix(GLOBAL_PREFIX)
+	app.use(compression())
+
+	await app.listen(3000)
 }
-bootstrap();
+bootstrap()
