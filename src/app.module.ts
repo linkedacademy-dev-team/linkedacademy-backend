e0eacd8a059c4e5d63810ba63d5788d05552e1e4
DQuintero15 <quinterod164@gmail.com>
2024-03-23 19:42:08 -0500

    feat: add inital config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common"
+import { ConfigModule } from "@nestjs/config"
+import { JwtModule } from "@nestjs/jwt"
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { typeormConfig } from "db/typeorm.config"
+
+import { AppController } from "./app.controller"
+import { AppService } from "./app.service"
+import { AuthModule } from "./auth/auth.module"
+import { envConfig, jwtConfig } from "./shared/config"
+import { SharedModule } from "./shared/shared.module"
+import { UsersModule } from "./users/users.module"
+import { LocationsModule } from './locations/locations.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(envConfig),
+		TypeOrmModule.forRoot(typeormConfig),
+		JwtModule.register(jwtConfig),
+		SharedModule,
+		AuthModule,
+		UsersModule,
+		LocationsModule
+	],
+	controllers: [AppController],
+	providers: [AppService]
 })
 export class AppModule {}
