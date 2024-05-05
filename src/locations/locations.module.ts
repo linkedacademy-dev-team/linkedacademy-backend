02e8673e2664fd0b84eddca44e20a15a329fa857
DQuintero15 <quinterod164@gmail.com>
2024-05-05 16:11:52 -0500

    feat: Add lazy loading for images in locations module

--- src/locations/locations.module.ts
+++ src/locations/locations.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common"
 import { TypeOrmModule } from "@nestjs/typeorm"
+import { SharedModule } from "src/shared/shared.module"
 
 import { CityController, CountryController, DepartamentController } from "./controllers"
 import { City, Country, Departament } from "./entities"
 import { CityService, CountryService, DepartamentService } from "./services"
 
 @Module({
-	imports: [TypeOrmModule.forFeature([City, Departament, Country])],
+	imports: [TypeOrmModule.forFeature([City, Departament, Country]), SharedModule],
 	controllers: [CityController, CountryController, DepartamentController],
 	providers: [CityService, DepartamentService, CountryService]
 })
