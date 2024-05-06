f76f2a757ee17e941c6fdeaaa28443e9aac93e5a
DQuintero15 <quinterod164@gmail.com>
2024-05-05 21:20:37 -0500

    feat: Update 'feature' table enum values in migration

--- db/migrations/1714959250590-add_other_entities_for_auth_module.ts
+++ db/migrations/1714959250590-add_other_entities_for_auth_module.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class AddOtherEntitiesForAuthModule1714959250590 implements MigrationInterface {
-    name = 'AddOtherEntitiesForAuthModule1714959250590'
+	name = "AddOtherEntitiesForAuthModule1714959250590"
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`feature\` CHANGE \`name\` \`name\` enum ('USERS', 'AUTH', 'COUNTRIES', 'DEPARTMENTS', 'CITIES', 'SCHOOLS', 'DISABILITIES', 'EDUCATION_LEVELS', 'EDUCATION_MODES', 'ETHNIC_GROUPS', 'LANGUAGES', 'SESSIONS', 'SPECIALITIES') NOT NULL`);
-    }
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`ALTER TABLE \`feature\` CHANGE \`name\` \`name\` enum ('USERS', 'AUTH', 'COUNTRIES', 'DEPARTMENTS', 'CITIES', 'SCHOOLS', 'DISABILITIES', 'EDUCATION_LEVELS', 'EDUCATION_MODES', 'ETHNIC_GROUPS', 'LANGUAGES', 'SESSIONS', 'SPECIALITIES') NOT NULL`
+		)
+	}
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`feature\` CHANGE \`name\` \`name\` enum ('USERS', 'AUTH', 'SCHOOLS') NOT NULL`);
-    }
-
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`ALTER TABLE \`feature\` CHANGE \`name\` \`name\` enum ('USERS', 'AUTH', 'SCHOOLS') NOT NULL`
+		)
+	}
 }
