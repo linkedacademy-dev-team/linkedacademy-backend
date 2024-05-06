59c1a7ca32c64fdf15f6d3492d727be6c486e61c
DQuintero15 <quinterod164@gmail.com>
2024-05-05 22:10:43 -0500

    feat: Add EthnicGroupController and EthnicGroupService

---
+++ src/schools/services/ethnic-group.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@nestjs/common"
+import { InjectRepository } from "@nestjs/typeorm"
+import { Repository } from "typeorm"
+
+import { CreateEthnicGroupDto } from "../dtos/ethnic-group/create-ethnic-group.dto"
+import { UpdateEthnicGroupDto } from "../dtos/ethnic-group/update-ethnic-group.dto"
+import { EthnicGroup } from "../entities"
+
+@Injectable()
+export class EthnicGroupService {
+	constructor(
+		@InjectRepository(EthnicGroup) private readonly ethnicGroupRepository: Repository<EthnicGroup>
+	) {}
+
+	async create(createEthnicGroupDto: CreateEthnicGroupDto) {
+		return this.ethnicGroupRepository.save(createEthnicGroupDto)
+	}
+
+	async getAll() {
+		return this.ethnicGroupRepository.find()
+	}
+
+	async update(id: number, updateEthnicGroupDto: UpdateEthnicGroupDto) {
+		return this.ethnicGroupRepository.update(id, updateEthnicGroupDto)
+	}
+
+	async delete(id: number) {
+		return this.ethnicGroupRepository.softDelete(id)
+	}
+}
