94e49ed89944494ff09c8c02459967a3cad873f8
DQuintero15 <quinterod164@gmail.com>
2024-05-07 15:51:24 -0500

    feat: Add convertKilometersToMeters method to GeolocationUtil

--- src/shared/utils/geolocation.util.ts
+++ src/shared/utils/geolocation.util.ts
@@ -2,6 +2,10 @@ import { Injectable } from "@nestjs/common"
 
 @Injectable()
 export class GeolocationUtil {
+	convertKilometersToMeters(kilometers: number): number {
+		return kilometers * 1000
+	}
+
 	normalizePointGeometry(point: string): number[] {
 		return point.replace("POINT(", "").replace(")", "").split(" ").map(Number)
 	}
