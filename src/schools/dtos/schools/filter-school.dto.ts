3968836604e251122a29ac8b2790969d95988851
DQuintero15 <quinterod164@gmail.com>
2024-05-07 15:52:28 -0500

    feat: Add FilterSchoolDto for filtering schools

---
+++ src/schools/dtos/schools/filter-school.dto.ts
@@ -0,0 +1,167 @@
+import { Type } from "class-transformer"
+import {
+	IsArray,
+	IsBoolean,
+	IsEnum,
+	IsNotEmpty,
+	IsNumber,
+	IsOptional,
+	IsString,
+	Max,
+	Min
+} from "class-validator"
+import { SCHOOL_GENDERS, SCHOOL_SCHEDULES, SCHOOL_TYPES, SCHOOL_ZONES } from "src/schools/constants"
+
+export class FilterSchoolDto {
+	@IsNumber()
+	@IsNotEmpty()
+	@Type(() => Number)
+	readonly cityId: number
+
+	@IsNumber()
+	@Min(0)
+	@Max(100)
+	@Type(() => Number)
+	readonly distance: number
+
+	@IsArray()
+	@Type(() => Number)
+	readonly coordinates: number[]
+
+	@IsOptional()
+	@IsString()
+	readonly name?: string
+
+	@IsOptional()
+	@IsEnum(SCHOOL_ZONES)
+	readonly zone?: SCHOOL_ZONES
+
+	@IsOptional()
+	@IsEnum(SCHOOL_SCHEDULES)
+	readonly schedule?: SCHOOL_SCHEDULES
+
+	@IsOptional()
+	@IsEnum(SCHOOL_GENDERS)
+	readonly gender?: SCHOOL_GENDERS
+
+	@IsOptional()
+	@IsEnum(SCHOOL_TYPES)
+	readonly type?: SCHOOL_TYPES
+
+	@IsOptional()
+	@IsNumber()
+	@Min(0)
+	readonly minBudget?: number
+
+	@IsOptional()
+	@IsNumber()
+	@Min(0)
+	@Type(() => Number)
+	readonly maxBudget?: number
+
+	@IsOptional()
+	@IsNumber()
+	@Min(0)
+	@Type(() => Number)
+	readonly studentsPerClass?: number
+
+	@IsOptional()
+	@IsBoolean()
+	@Type(() => Boolean)
+	readonly withLaboratories?: boolean
+
+	@IsOptional()
+	@IsBoolean()
+	@Type(() => Boolean)
+	readonly withLibrary?: boolean
+
+	@IsOptional()
+	@IsBoolean()
+	@Type(() => Boolean)
+	readonly withInternet?: boolean
+
+	@IsOptional()
+	@IsBoolean()
+	@Type(() => Boolean)
+	readonly withSoccerField?: boolean
+
+	@IsOptional()
+	@IsBoolean()
+	@Type(() => Boolean)
+	readonly withVolleyballField?: boolean
+
+	@IsOptional()
+	@IsBoolean()
+	@Type(() => Boolean)
+	readonly withBasketballField?: boolean
+
+	@IsOptional()
+	@IsBoolean()
+	@Type(() => Boolean)
+	readonly withSwimmingPool?: boolean
+
+	@IsOptional()
+	@IsBoolean()
+	@Type(() => Boolean)
+	readonly withComputerLab?: boolean
+
+	@IsOptional()
+	@IsBoolean()
+	@Type(() => Boolean)
+	readonly withTransport?: boolean
+
+	@IsOptional()
+	@IsBoolean()
+	@Type(() => Boolean)
+	readonly withRestaurant?: boolean
+
+	@IsOptional()
+	@IsBoolean()
+	@Type(() => Boolean)
+	readonly withAuditorium?: boolean
+
+	@IsOptional()
+	@IsBoolean()
+	@Type(() => Boolean)
+	readonly withSnackArea?: boolean
+
+	@IsOptional()
+	@IsBoolean()
+	@Type(() => Boolean)
+	readonly withChildren?: boolean
+
+	@IsOptional()
+	@IsArray()
+	@Type(() => Number)
+	readonly languages?: number[]
+
+	@IsOptional()
+	@IsArray()
+	@Type(() => Number)
+	readonly specialities?: number[]
+
+	@IsOptional()
+	@IsArray()
+	@Type(() => Number)
+	readonly levels?: number[]
+
+	@IsOptional()
+	@IsArray()
+	@Type(() => Number)
+	readonly sessions?: number[]
+
+	@IsOptional()
+	@IsArray()
+	@Type(() => Number)
+	readonly disabilities?: number[]
+
+	@IsOptional()
+	@IsArray()
+	@Type(() => Number)
+	readonly models?: number[]
+
+	@IsOptional()
+	@IsArray()
+	@Type(() => Number)
+	readonly ethnicGroups?: number[]
+}
--- src/schools/services/schools.service.ts
+++ src/schools/services/schools.service.ts
@@ -1,13 +1,132 @@
 import { Injectable } from "@nestjs/common"
 import { InjectRepository } from "@nestjs/typeorm"
+import { GeolocationUtil } from "src/shared/utils"
 import { Repository } from "typeorm"
 
 import { CreateSchoolDto } from "../dtos"
+import { FilterSchoolDto } from "../dtos/schools/filter-school.dto"
 import { School } from "../entities"
 
 @Injectable()
 export class SchoolsService {
-	constructor(@InjectRepository(School) private readonly schoolRepository: Repository<School>) {}
+	constructor(
+		@InjectRepository(School) private readonly schoolRepository: Repository<School>,
+		private readonly geolocationUtil: GeolocationUtil
+	) {}
+
+	async filter(filter: FilterSchoolDto) {
+		const { coordinates, distance, cityId } = filter
+
+		const numericCoordinates = coordinates?.map((coordinate) => +coordinate)
+
+		const schools = this.schoolRepository
+			.createQueryBuilder("school")
+			.where("school.cityId = :cityId", { cityId })
+			.andWhere(
+				"ST_Distance_Sphere(school.coordinates, POINT(:longitude, :latitude)) <= :distance",
+				{
+					longitude: numericCoordinates[0],
+					latitude: numericCoordinates[1],
+					distance: this.geolocationUtil.convertKilometersToMeters(distance)
+				}
+			)
+
+		if (filter.name) {
+			schools.andWhere("school.name ILIKE :name", { name: `%${filter.name}%` })
+		}
+
+		if (filter.zone) {
+			schools.andWhere("school.zone = :zone", { zone: filter.zone })
+		}
+
+		if (filter.schedule) {
+			schools.andWhere("school.schedule = :schedule", { schedule: filter.schedule })
+		}
+
+		if (filter.gender) {
+			schools.andWhere("school.gender = :gender", { gender: filter.gender })
+		}
+
+		if (filter.type) {
+			schools.andWhere("school.type = :type", { type: filter.type })
+		}
+
+		if (filter.studentsPerClass) {
+			schools.andWhere("school.studentsPerClass = :students", {
+				students: filter.studentsPerClass
+			})
+		}
+
+		if (filter.withLaboratories) {
+			schools.andWhere("school.withLaboratories = :withLaboratories", {
+				withLaboratories: filter.withLaboratories
+			})
+		}
+
+		if (filter.withLibrary) {
+			schools.andWhere("school.withLibrary = :withLibrary", { withLibrary: filter.withLibrary })
+		}
+
+		if (filter.withInternet) {
+			schools.andWhere("school.withInternet = :withInternet", { withInternet: filter.withInternet })
+		}
+
+		if (filter.withSoccerField) {
+			schools.andWhere("school.withSoccerField = :withSoccerField", {
+				withSoccerField: filter.withSoccerField
+			})
+		}
+
+		if (filter.withVolleyballField) {
+			schools.andWhere("school.withVolleyballField = :withVolleyballField", {
+				withVolleyballField: filter.withVolleyballField
+			})
+		}
+
+		if (filter.withBasketballField) {
+			schools.andWhere("school.withBasketballField = :withBasketballField", {
+				withBasketballField: filter.withBasketballField
+			})
+		}
+
+		if (filter.withSwimmingPool) {
+			schools.andWhere("school.withSwimmingPool = :withSwimmingPool", {
+				withSwimmingPool: filter.withSwimmingPool
+			})
+		}
+
+		if (filter.withComputerLab) {
+			schools.andWhere("school.withComputerLab = :withComputerLab", {
+				withComputerLab: filter.withComputerLab
+			})
+		}
+
+		if (filter.withTransport) {
+			schools.andWhere("school.withTransport = :withTransport", {
+				withTransport: filter.withTransport
+			})
+		}
+
+		if (filter.withRestaurant) {
+			schools.andWhere("school.withRestaurant = :withRestaurant", {
+				withRestaurant: filter.withRestaurant
+			})
+		}
+
+		if (filter.withAuditorium) {
+			schools.andWhere("school.withAuditorium = :withAuditorium", {
+				withAuditorium: filter.withAuditorium
+			})
+		}
+
+		if (filter.withSnackArea) {
+			schools.andWhere("school.withSnackArea = :withSnackArea", {
+				withSnackArea: filter.withSnackArea
+			})
+		}
+
+		return await schools.getMany()
+	}
 
 	async create(createSchoolDto: CreateSchoolDto): Promise<void> {
 		const { coordinates, ...rest } = createSchoolDto
