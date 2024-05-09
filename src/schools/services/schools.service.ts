c55bede50b2eb2d07f4edc26a024559c7ff1f4d9
DQuintero15 <quinterod164@gmail.com>
2024-05-09 01:38:22 -0500

    feat: Refactor filter method in SchoolsService to improve readability and maintainability

--- src/schools/services/schools.service.ts
+++ src/schools/services/schools.service.ts
@@ -15,114 +15,24 @@ export class SchoolsService {
 	) {}
 
 	async filter(filter: FilterSchoolDto) {
-		const { coordinates, distance, cityId } = filter
+		const { coordinates, distance, cityId, ...rest } = filter
 
-		const numericCoordinates = coordinates?.map((coordinate) => +coordinate)
+		const [latitude, longitude] = coordinates.map((coordinate) => +coordinate)
+		const distanceInMeters = this.geolocationUtil.convertKilometersToMeters(distance)
 
 		const schools = this.schoolRepository
 			.createQueryBuilder("school")
 			.where("school.cityId = :cityId", { cityId })
-			.andWhere(
-				"ST_Distance_Sphere(school.coordinates, POINT(:longitude, :latitude)) <= :distance",
-				{
-					longitude: numericCoordinates[0],
-					latitude: numericCoordinates[1],
-					distance: this.geolocationUtil.convertKilometersToMeters(distance)
-				}
-			)
-
-		if (filter.name) {
-			schools.andWhere("school.name ILIKE :name", { name: `%${filter.name}%` })
-		}
-
-		if (filter.zone) {
-			schools.andWhere("school.zone = :zone", { zone: filter.zone })
-		}
-
-		if (filter.schedule) {
-			schools.andWhere("school.schedule = :schedule", { schedule: filter.schedule })
-		}
-
-		if (filter.gender) {
-			schools.andWhere("school.gender = :gender", { gender: filter.gender })
-		}
-
-		if (filter.type) {
-			schools.andWhere("school.type = :type", { type: filter.type })
-		}
-
-		if (filter.studentsPerClass) {
-			schools.andWhere("school.studentsPerClass = :students", {
-				students: filter.studentsPerClass
+			.andWhere("is_within_radius(coordinates,:latitude, :longitude, :distance) = 1", {
+				longitude,
+				latitude,
+				distance: distanceInMeters
 			})
-		}
 
-		if (filter.withLaboratories) {
-			schools.andWhere("school.withLaboratories = :withLaboratories", {
-				withLaboratories: filter.withLaboratories
-			})
-		}
-
-		if (filter.withLibrary) {
-			schools.andWhere("school.withLibrary = :withLibrary", { withLibrary: filter.withLibrary })
-		}
-
-		if (filter.withInternet) {
-			schools.andWhere("school.withInternet = :withInternet", { withInternet: filter.withInternet })
-		}
-
-		if (filter.withSoccerField) {
-			schools.andWhere("school.withSoccerField = :withSoccerField", {
-				withSoccerField: filter.withSoccerField
-			})
-		}
-
-		if (filter.withVolleyballField) {
-			schools.andWhere("school.withVolleyballField = :withVolleyballField", {
-				withVolleyballField: filter.withVolleyballField
-			})
-		}
-
-		if (filter.withBasketballField) {
-			schools.andWhere("school.withBasketballField = :withBasketballField", {
-				withBasketballField: filter.withBasketballField
-			})
-		}
-
-		if (filter.withSwimmingPool) {
-			schools.andWhere("school.withSwimmingPool = :withSwimmingPool", {
-				withSwimmingPool: filter.withSwimmingPool
-			})
-		}
-
-		if (filter.withComputerLab) {
-			schools.andWhere("school.withComputerLab = :withComputerLab", {
-				withComputerLab: filter.withComputerLab
-			})
-		}
-
-		if (filter.withTransport) {
-			schools.andWhere("school.withTransport = :withTransport", {
-				withTransport: filter.withTransport
-			})
-		}
-
-		if (filter.withRestaurant) {
-			schools.andWhere("school.withRestaurant = :withRestaurant", {
-				withRestaurant: filter.withRestaurant
-			})
-		}
-
-		if (filter.withAuditorium) {
-			schools.andWhere("school.withAuditorium = :withAuditorium", {
-				withAuditorium: filter.withAuditorium
-			})
-		}
-
-		if (filter.withSnackArea) {
-			schools.andWhere("school.withSnackArea = :withSnackArea", {
-				withSnackArea: filter.withSnackArea
-			})
+		for (const key in rest) {
+			if (filter[key]) {
+				schools.andWhere(`school.${key} = :${key}`, { [key]: filter[key] })
+			}
 		}
 
 		return await schools.getMany()
