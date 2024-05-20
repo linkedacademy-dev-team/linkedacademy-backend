7bece55f27b06d334ee64a1f36607f1254cf9722
DQuintero15 <quinterod164@gmail.com>
2024-05-20 03:21:17 -0500

    refactor: Improve readability and performance of filter method in SchoolsService

--- src/shared/utils/geolocation.util.ts
+++ src/shared/utils/geolocation.util.ts
@@ -2,10 +2,6 @@ import { Injectable } from "@nestjs/common"
 
 @Injectable()
 export class GeolocationUtil {
-	convertKilometersToMeters(kilometers: number): number {
-		return kilometers * 1000
-	}
-
 	normalizePointGeometry(point: string): number[] {
 		return point.replace("POINT(", "").replace(")", "").split(" ").map(Number)
 	}
