8d2985acc9be4f2b28b0a384c0262514e57fa0da
DQuintero15 <quinterod164@gmail.com>
2024-05-22 08:01:04 -0500

    feat: Add getProfile endpoint to UsersController

--- src/users/services/user.service.ts
+++ src/users/services/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common"
+import { Injectable, NotFoundException } from "@nestjs/common"
 import { InjectRepository } from "@nestjs/typeorm"
 import { Repository } from "typeorm"
 
@@ -7,6 +7,16 @@ import { User } from "../entites"
 @Injectable()
 export class UserService {
 	constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {}
+
+	async getProfile(id: number) {
+		const user = await this.findOneById(id)
+
+		if (!user) throw new NotFoundException("User not found")
+
+		const { firstName, lastName, id: userId, email } = user
+
+		return { firstName, lastName, id: userId, email }
+	}
 
 	async findOneById(id: number): Promise<User> {
 		return await this.userRepository.findOne({ where: { id } })
